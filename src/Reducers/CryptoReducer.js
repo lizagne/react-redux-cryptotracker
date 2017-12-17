9f2a847d9829fbc828a1f5c8d9e066ce5c62b2f3
Liz Verde <>
2017-12-17 17:16:41 +0000

    Adding the switch statements to the Cryptoreducer

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -1,6 +1,41 @@
 //reducers are asynchronous by default — that’s why they are anonymous functions — all reducers get invoked after an action. That’s why we have the switch conditional to handle appropriate actions.
 
+import {
+	FETCHING_COIN_DATA,
+	FETCHING_COIN_DATA_SUCCESS,
+	FETCHING_COIN_DATA_FAIL
+} from './../Utils/ActionTypes';
 
-export default CryptoReducer = (state = [], action) => {
-	return state;
+const initialState = {
+	isFetching: false,
+	data: null,
+	hasError: false,
+	errorMessage: null
+}
+
+export default CryptoReducer = (state = initialState, action) => {
+
+	switch(action.type) {
+		case FETCHING_COIN_DATA:
+
+			return { 
+				...state,
+				isFetching: true, 
+			}
+
+		case FETCHING_COIN_DATA_SUCCESS:
+			return {
+				...state,
+				data: action.payload,				
+			}
+
+		case FETCHING_COIN_DATA_FAIL:
+			return {
+				...state,
+				hasError: true,
+			}
+
+		default:
+			return state;
+	}
 }
